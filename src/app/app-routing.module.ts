deb788aa3a9e1c0019b5306132883acc72dc73e6
johnjoma <irungujohnkimani@gmail.com>
2020-02-24 10:47:34 +0300

    New

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+
+  },
+
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+
+  {
+    path: 'users',
+
+    loadChildren: () =>
+      import('./users/users.module').then(m => m.UsersModule)
+  }
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+  name: string;
+  email: string;
+  message: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  submitForm() {
+    alert('I am submitting shitty form! 😆 😆');
+  }
+
+}
